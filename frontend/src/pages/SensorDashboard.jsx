b503c792dc1935ea3a19002df857876be77bc34e
B V Harshith Gowda <harshith200666@gmail.com>
2026-05-25 20:50:16 +0530

    Update SensorDashboard.jsx

--- frontend/src/pages/SensorDashboard.jsx
+++ frontend/src/pages/SensorDashboard.jsx
@@ -64,7 +64,7 @@ export default function SensorDashboard() {
     try {
 
       // FIXED API ENDPOINT
-      const res = await axios.get(`${BACKEND}/data`)
+      const res = await axios.get(`${BACKEND}/sensor-latest`)
 
       console.log("Sensor API Response:", res.data)
 
